3bf8595930b7b017d413b755efad1944c1a67f81
Sung Noh <noh.sung@gmail.com>
2024-06-29 16:07:20 +0000

    Implement challenge 2
    Generate a message to handle unique ID creation produced from the hash
    of the node id, client id, and timestamp.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+use std::fmt;
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::time::SystemTime;
+
 use std::io::{self, BufRead, BufReader, Read};
 
 #[derive(Clone, Debug, Deserialize)]
@@ -14,6 +18,7 @@ struct Message {
 enum MessageBody {
     Init(InitBody),
     Echo(EchoBody),
+    Generate(GenerateBody),
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -34,11 +39,19 @@ struct EchoBody {
     echo: String,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct GenerateBody {
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    r#type: String,
+    msg_id: Option<i32>,
+}
+
 #[derive(Debug, Deserialize)]
 enum MessageKind {
     Init(Message),
     Echo(Message),
     Invalid(Message),
+    Generate(Message),
 }
 
 #[derive(Debug, Serialize)]
@@ -46,6 +59,7 @@ enum MessageKind {
 enum Response {
     InitOk(InitOkResponse),
     EchoOk(EchoOkResponse),
+    GenerateOk(GenerateOkResponse),
     Invalid(InvalidResponse),
 }
 
@@ -80,6 +94,22 @@ struct EchoOkBody {
     echo: String,
 }
 
+#[derive(Clone, Debug, Serialize)]
+struct GenerateOkResponse {
+    src: Option<String>,
+    dest: String,
+    body: GenerateOkBody,
+}
+
+#[derive(Clone, Debug, Serialize)]
+struct GenerateOkBody {
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    r#type: String,
+    msg_id: Option<i32>,
+    in_reply_to: Option<i32>,
+    id: u64,
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct InvalidResponse {
     src: Option<String>,
@@ -87,11 +117,29 @@ struct InvalidResponse {
     body: String,
 }
 
+struct Node {
+    id: Option<String>,
+}
+
+#[derive(Clone, Hash, Serialize)]
+struct UniqueId(String);
+
+impl UniqueId {
+    fn generate_hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+
+        self.0.hash(&mut hasher);
+
+        hasher.finish()
+    }
+}
+
 impl Message {
     fn new(message: Message) -> MessageKind {
         return match message.body {
             MessageBody::Init(ref _body) => MessageKind::Init(message),
             MessageBody::Echo(ref _body) => MessageKind::Echo(message),
+            MessageBody::Generate(ref _body) => MessageKind::Generate(message),
             _ => MessageKind::Invalid(message),
         };
     }
@@ -151,6 +199,37 @@ impl MessageKind {
                     message,
                 )
             }
+            MessageKind::Generate(message) => {
+                let MessageBody::Generate(body) = &message.body else {
+                    return (
+                        Response::Invalid(InvalidResponse {
+                            src: node.id.clone(),
+                            dest: message.clone().src.unwrap().to_owned(),
+                            body: "There was an error.".to_string(),
+                        }),
+                        message,
+                    );
+                };
+
+                (
+                    Response::GenerateOk(GenerateOkResponse {
+                        src: node.id.clone(),
+                        dest: message.clone().src.unwrap().to_owned(),
+                        body: GenerateOkBody {
+                            r#type: "generate_ok".to_string(),
+                            in_reply_to: body.msg_id,
+                            msg_id: body.msg_id.and_then(|id| Some(id + 1)),
+                            id: node.generate_uuid(
+                                &message
+                                    .to_owned()
+                                    .src
+                                    .expect("Generate message did not include a src."),
+                            ),
+                        },
+                    }),
+                    message,
+                )
+            }
             MessageKind::Invalid(message) => (
                 Response::Invalid(InvalidResponse {
                     src: node.id.clone(),
@@ -161,10 +240,6 @@ impl MessageKind {
             ),
         };
     }
-}
-
-struct Node {
-    id: Option<String>,
 }
 
 impl Node {
@@ -201,6 +276,18 @@ impl Node {
             self.id = Some(body.node_id.to_owned());
         }
     }
+
+    fn generate_uuid(&self, client_id: &String) -> u64 {
+        let time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+
+        if let Some(ref id) = self.id {
+            UniqueId(format!("{}-{}-{}", id, client_id, time.as_nanos())).generate_hash()
+        } else {
+            panic!()
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -222,6 +309,18 @@ mod test {
         let mut node = Node { id: None };
 
         let mut message = r#"{"id": 508799, "src": "c1", "dest": "n3", "body": {"type": "init", "msg_id": 1, "in_reply_to": null, "node_id": "n1", "node_ids": ["n1", "n2", "n3"]}}"#.as_bytes();
+
+        let _ = node.run(&mut message, &mut buffer);
+    }
+
+    #[test]
+    fn responds_to_generate_message() {
+        let mut buffer = String::new();
+        let mut node = Node {
+            id: Some("n1".to_string()),
+        };
+
+        let mut message = r#"{"id": 500005, "src": "c1", "dest": "n3", "body": {"type": "generate", "msg_id": 1 }}"#.as_bytes();
 
         let _ = node.run(&mut message, &mut buffer);
     }
