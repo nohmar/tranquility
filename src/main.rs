1a6a7fce574021e0db116fa6b14a620463fdd29b
Sung Noh <noh.sung@gmail.com>
2024-07-06 01:16:53 +0000

    Move callback behavior to `MessageKind` impl
    Temporarily move message callbacks to the `MessageKind` implementation
    to tidy things up a bit. It would've been nice to be able to define a
    trait implemented by every `MessageKind` variant, however, Rust doesn't
    allow you to do that since enum variant's aren't actual types.

--- src/main.rs
+++ src/main.rs
@@ -413,6 +413,89 @@ impl MessageKind {
             ),
         };
     }
+
+    fn run_callback(&self, node: &mut Node) {
+        match self {
+            MessageKind::Init(message) => {
+                if let MessageBody::Init(body) = &message.body {
+                    node.id = Some(body.node_id.to_owned());
+                }
+            }
+            MessageKind::Broadcast(message) => {
+                if let MessageBody::Broadcast(body) = &message.body {
+                    let is_message_seen = node.messages.contains(&body.message);
+
+                    if body.in_reply_to.is_some() {
+                        if let Some(response_callback) =
+                            node.response_callbacks.remove(&body.in_reply_to.unwrap())
+                        {
+                            let ResponseCallback(callback) = response_callback;
+                            callback()
+                        }
+                    }
+
+                    if !is_message_seen {
+                        node.messages.insert(body.message);
+
+                        // Broadcast the message to every known node, expect to itself, and the one that sent the message.
+                        for (node_id, neighbors) in node.topology.clone().iter() {
+                            if *node_id != node.id.clone().unwrap() {
+                                continue;
+                            }
+
+                            for neighbor in neighbors {
+                                let stdout = io::stdout();
+                                let mut lock = stdout.lock();
+
+                                let next_message_id = node.next_message_id();
+                                let body_clone = body.clone();
+
+                                let message = Message {
+                                    src: node.id.clone(),
+                                    dest: neighbor.to_owned(),
+                                    body: MessageBody::Broadcast(BroadcastBody {
+                                        r#type: "broadcast".to_owned(),
+                                        msg_id: Some(next_message_id),
+                                        in_reply_to: None,
+                                        message: body_clone.message,
+                                    }),
+                                };
+
+                                // Flush the message to stdout.
+                                writeln!(
+                                    lock,
+                                    "{}",
+                                    serde_json::to_string(&message)
+                                        .expect("Couldn't parse message.")
+                                )
+                                .unwrap();
+
+                                // Add a callback for the message, using the message id as the key.
+                                node.response_callbacks.insert(
+                                    next_message_id,
+                                    ResponseCallback(Box::new(move || {
+                                        eprintln!(
+                                            "Callback invoked for msg: {}",
+                                            body_clone.msg_id.unwrap()
+                                        );
+                                    })),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            MessageKind::Topology(message) => {
+                if let MessageBody::Topology(body) = &message.body {
+                    node.topology = body.topology.to_owned();
+                }
+            }
+            MessageKind::Read(_message) => (),
+            MessageKind::Generate(_message) => (),
+            MessageKind::Invalid(_message) => (),
+            MessageKind::Echo(_message) => (),
+        }
+    }
 }
 
 impl Node {
@@ -425,9 +508,9 @@ impl Node {
             let _ = input.read_line(output);
 
             let serialized_message: Message = serde_json::from_str(&output)?;
-            self.before_response_callback(&serialized_message);
 
             let message = Message::new(serialized_message);
+            message.run_callback(self);
 
             let (response, _original_message) = message.generate_response(self);
 
@@ -452,78 +535,6 @@ impl Node {
             // Flush all buffers
             output.clear();
             io::stdout().flush()?;
-        }
-    }
-
-    fn before_response_callback(&mut self, message: &Message) {
-        if let MessageBody::Init(body) = &message.body {
-            self.id = Some(body.node_id.to_owned());
-        }
-
-        if let MessageBody::Broadcast(body) = &message.body {
-            let is_message_seen = self.messages.contains(&body.message);
-
-            if body.in_reply_to.is_some() {
-                if let Some(response_callback) =
-                    self.response_callbacks.remove(&body.in_reply_to.unwrap())
-                {
-                    let ResponseCallback(callback) = response_callback;
-                    callback()
-                }
-            }
-
-            if !is_message_seen {
-                self.messages.insert(body.message);
-
-                // Broadcast the message to every known node, expect to itself, and the one that sent the message.
-                for (node_id, neighbors) in self.topology.clone().iter() {
-                    if *node_id != self.id.clone().unwrap() {
-                        continue;
-                    }
-
-                    for neighbor in neighbors {
-                        let stdout = io::stdout();
-                        let mut lock = stdout.lock();
-
-                        let next_message_id = self.next_message_id();
-                        let body_clone = body.clone();
-
-                        let message = Message {
-                            src: self.id.clone(),
-                            dest: neighbor.to_owned(),
-                            body: MessageBody::Broadcast(BroadcastBody {
-                                r#type: "broadcast".to_owned(),
-                                msg_id: Some(next_message_id),
-                                in_reply_to: None,
-                                message: body_clone.message,
-                            }),
-                        };
-
-                        // Flush the message to stdout.
-                        writeln!(
-                            lock,
-                            "{}",
-                            serde_json::to_string(&message).expect("Couldn't parse message.")
-                        )
-                        .unwrap();
-
-                        // Add a callback for the message, using the message id as the key.
-                        self.response_callbacks.insert(
-                            next_message_id,
-                            ResponseCallback(Box::new(move || {
-                                eprintln!(
-                                    "Callback invoked for msg: {}",
-                                    body_clone.msg_id.unwrap()
-                                );
-                            })),
-                        );
-                    }
-                }
-            }
-        }
-
-        if let MessageBody::Topology(body) = &message.body {
-            self.topology = body.topology.to_owned();
         }
     }
 
